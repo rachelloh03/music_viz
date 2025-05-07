ee1e2963f92db139515b3af5fdb73b775e664821
Rachel <rjloh@mit.edu>
2025-05-07 00:50:38 -0400

    draw arcs instead of lines

--- music-viz-app/app/drawArc/drawArc.ts
+++ music-viz-app/app/drawArc/drawArc.ts
@@ -22,43 +22,32 @@ export const drawArc = (
   const y1 = getY(pastNote.pitch, canvas);
   const y2 = getY(curNote.pitch, canvas);
 
-  //   const circleX = (x2 + x1) / 2;
-  if (y1 && y2 && x2 > 0 && x2 <= canvas.width) {
-    // const radius = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2) / 2;
-    // const circleY = (y1 + y2) / 2;
-    // const startAngle =
-    //   Math.tan(Math.abs(y1 - circleY) / Math.abs(x1 - circleX)) *
-    //   getSign(x1, y1, circleX, circleY);
-    // const endAngle =
-    //   Math.tan(Math.abs(y2 - circleY) / Math.abs(x2 - circleX)) *
-    //     getSign(x2, y2, circleX, circleY) +
-    //   Math.PI;
+  const endTime = curNote.endTime;
+  const curEndTime = !endTime ? curTime : endTime;
+  const curNoteWidth = (curEndTime - curNote.startTime) * speed;
 
-    // ctx.beginPath();
-    // ctx.arc(circleX, circleY, radius, startAngle, endAngle);
-    // ctx.fillStyle = "green";
-    // ctx.stroke();
-
-    const endTime = curNote.endTime;
-    const curEndTime = !endTime ? curTime : endTime;
-    const curNoteWidth = (curEndTime - curNote.startTime) * speed * 6;
-
-    // if (x2 > canvas.width - curNoteWidth) {
-
-    ctx.lineWidth = 3;
+  const circleX = (x2 + x1) / 2;
+  if (
+    y1 &&
+    y2 &&
+    x2 >= canvas.width - curNoteWidth &&
+    x2 <= canvas.width &&
+    x1 >= 0
+  ) {
+    const radius = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2) / 2;
+    const circleY = (y1 + y2) / 2;
+    const startAngle = Math.atan2(y1 - circleY, x1 - circleX);
+    const endAngle = Math.atan2(y2 - circleY, x2 - circleX);
     ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
+    ctx.arc(circleX, circleY, radius, startAngle, endAngle);
+    ctx.fillStyle = "green";
     ctx.stroke();
-    // }
-  }
-};
 
-// returns the sign of the tangent value
-const getSign = (x: number, y: number, xbar: number, ybar: number) => {
-  if ((x > xbar && y < ybar) || (x < xbar && y > ybar)) {
-    return -1;
-  } else {
-    return 1;
+    // uncomment if want to draw lines instead of arcs
+    // ctx.lineWidth = 3;
+    // ctx.beginPath();
+    // ctx.moveTo(x1, y1);
+    // ctx.lineTo(x2, y2);
+    // ctx.stroke();
   }
 };
